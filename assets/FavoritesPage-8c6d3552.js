d02face9e05b73bf35e29a9341fdd0c41edbe816
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-03-08 04:01:45 +0000

    Deploying to gh-pages from @ NataliaMahera/test-task-rental-cars@ffa8fe9bee0ba9fb1e81cdc0496b5e606aec5a49 🚀

---
+++ assets/FavoritesPage-8c6d3552.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a5047e21.js";const t=()=>e.jsx("div",{children:"Favorite page"});export{t as default};
---
+++ assets/HomePage-0734e240.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a5047e21.js";const r=()=>e.jsx("div",{children:"Home page"});export{r as default};
