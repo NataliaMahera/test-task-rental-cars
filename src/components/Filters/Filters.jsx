e7c655c19a8b555d5ccb877f8c6b5b256a994914
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-03-11 07:13:11 +0100

    update arrow

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -6,10 +6,13 @@ import { selectAdverts } from '../../redux/adverts/advertsSelectors';
 import { FilterButton } from '../ReUseComponents/Buttons/Buttons';
 import { FilterForm } from './FilterStyled';
 import { Notify } from 'notiflix';
+import { ReactComponent as ArrowSvgDown } from '../../assets/icons/chevron-down.svg';
+import { ReactComponent as ArrowSvgUp } from '../../assets/icons/chevron-up.svg';
 
 const Filters = ({ setFilteredAdv }) => {
   const adverts = useSelector(selectAdverts);
-
+  const [focusBrand, setFokusBrand] = useState(null);
+  const [focusPrice, setFokusPrice] = useState(null);
   const [selectedValue, setSelectedValue] = useState({
     brand: '',
     price: '',
@@ -21,6 +24,8 @@ const Filters = ({ setFilteredAdv }) => {
       ...selectedValue,
       [name]: value,
     });
+    setFokusBrand(false);
+    setFokusPrice(false);
   };
 
   const handleSubmit = (e) => {
@@ -66,7 +71,7 @@ const Filters = ({ setFilteredAdv }) => {
         className="flex gap-x-[18px] items-end"
         onSubmit={handleSubmit}
       >
-        <div className="flex flex-col justify-center">
+        <div className="relative flex flex-col justify-center">
           <label
             className="text-[14px] leading-[1.29] font-medium text-label-color mb-[8px]"
             htmlFor="brand"
@@ -74,11 +79,17 @@ const Filters = ({ setFilteredAdv }) => {
             Car brand
           </label>
           <select
-            className="cursor-pointer text-[18px] leading-[1.11] text-primary-text-color font-medium outline-none h-[48px] w-[224px] rounded-[14px] px-[18px] bg-background-input-color"
+            className="appearance-none cursor-pointer text-[18px] leading-[1.11] text-primary-text-color font-medium outline-none h-[48px] w-[224px] rounded-[14px] px-[18px] bg-background-input-color"
             name="brand"
             value={selectedValue.brand}
             id="brand"
             onChange={handleChange}
+            onFocus={() => {
+              setFokusBrand(true);
+            }}
+            onBlur={() => {
+              setFokusBrand(false);
+            }}
           >
             <option className="rounded-[14px]" value="default">
               Enter the text
@@ -92,9 +103,14 @@ const Filters = ({ setFilteredAdv }) => {
               );
             })}
           </select>
+          {focusBrand ? (
+            <ArrowSvgDown className="arrow" />
+          ) : (
+            <ArrowSvgUp className="arrow" />
+          )}
         </div>
 
-        <div className="flex flex-col justify-center">
+        <div className="relative flex flex-col justify-center">
           <label
             className="text-[14px] leading-[1.29] font-medium text-label-color mb-[8px]"
             htmlFor="brand"
@@ -102,11 +118,17 @@ const Filters = ({ setFilteredAdv }) => {
             Price/ 1 hour
           </label>
           <select
-            className="cursor-pointer text-[18px] leading-[1.11] text-primary-text-color font-medium outline-none h-[48px] w-[125px] rounded-[14px] px-[18px] bg-background-input-color"
+            className="appearance-none cursor-pointer text-[18px] leading-[1.11] text-primary-text-color font-medium outline-none h-[48px] w-[125px] rounded-[14px] px-[18px] bg-background-input-color"
             value={selectedValue.price}
             name="price"
             id="brand"
             onChange={handleChange}
+            onFocus={() => {
+              setFokusPrice(true);
+            }}
+            onBlur={() => {
+              setFokusPrice(false);
+            }}
           >
             <option
               className="text-[16px] leading-[1.25] text-after-desc-element rounded-[14px]"
@@ -127,6 +149,11 @@ const Filters = ({ setFilteredAdv }) => {
               );
             })}
           </select>
+          {focusPrice ? (
+            <ArrowSvgDown className="arrow" />
+          ) : (
+            <ArrowSvgUp className="arrow" />
+          )}
         </div>
         <FilterButton type="submit">Search</FilterButton>
         <FilterButton type="button" onClick={clearFilters}>
